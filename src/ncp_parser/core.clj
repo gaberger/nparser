824033114b38d91703de4ffbe2bcf9d76fe2a339
Gary Berger <58652+gaberger@users.noreply.github.com>
2018-05-21 09:04:29 -0400

    Finish preliminary parser and transforms
    Signed-off-by: Gary Berger <58652+gaberger@users.noreply.github.com>

--- src/ncp_parser/core.clj
+++ src/ncp_parser/core.clj
@@ -31,39 +31,64 @@
   (pprint arg)
   (println (count arg))
   (println (type arg))
-  (into [] arg))
+  (assoc {} :bgp-med (into [] arg)))
+
   ; (reduce conj [] arg))
 
-(mapcat)
+(pprint (bgp-transform (frr (slurp (io/resource "configs/router1.cfg")))))
+
+
 (defn bgp-transform [input]
   (insta/transform
     {
         :ip-address                (comp #(conj [] :ip-address %) str)
-        :interface                 (fn bgprouter [& arg]
+        :bgp-med                   (fn bgp-med [& arg]
+                                     (assoc {} :bgp-med (into [] arg)))
+        :bgp-bestpath              (fn bgp-bestpath [& arg]
+                                       (assoc {} :bgp-bestpath (into [] arg)))
+        :interface                 (fn interface [& arg]
                                      (reduce conj {} arg))
         :interface-list            (fn interface-list [& arg]
-                                       (assoc {} :interface-list (into [] arg)))}
-        ; :neighbor                 (fn neighbor [& arg]
-        ;                              (reduce conj {} arg))}
-        ; :neighbor-list            (fn neighbor-list [& arg]
-        ;                             (assoc {} :neighbor-list (into [] arg)))}
+                                       (assoc {} :interface-list (into [] arg)))
+        :ip-prefix-list            (fn ip-prefix-list [& arg]
+                                       (assoc {} :ip-prefix-list (into [] arg)))
+        :ip-prefix                 (comp #(conj [] :ip-prefix %) str)
+        :ip-prefix-record          (fn ip-prefix-record [& arg]
+                                     (reduce conj {} arg))
+        :community                 (comp #(conj [] :community %) str)
 
-
-        ; :bgprouter                 (fn bgprouter [& arg]
-        ;                              (assoc {} :bgprouter (reduce conj {} arg)))
-        ; :asn                       (fn asn [arg]
-        ;                               (let [asnval (clojure.edn/read-string arg)]
-        ;                                {:asn asnval}))
-        ; :bgp-global                (fn c [& arg]
-        ;                              (assoc {} :bgp-global (reduce conj {} arg)))
-        ; :bgp-med                   (fn c [& arg]
-        ;                              (assoc {} :bgp-med (into [] arg)))
-        ; :bgp-bestpath               (fn c [& arg]
-        ;                              (assoc {} :bgp-bestpath (into [] arg)))
-        ; :bgp-confederation          (fn c [& arg]
-        ;                               (assoc {} :bgp-confederation (reduce conj {} arg)))
-        ; :confederation-peers        (fn cp [& arg]
-        ;                               (conj [] :confederation-peers (into #{} (map #(clojure.edn/read-string %) arg))))
-        ; :confederation-identifier   (fn ci [arg]
-        ;                                {:confederation-identifier  (clojure.edn/read-string arg)})}
+        :ip-community-list-standard  (fn icls [& arg]
+                                          (assoc {} :ip-community-list-standard (reduce conj {} arg)))
+        :ip-community-list-extended  (fn icle [& arg]
+                                          (assoc {} :ip-community-list-extended (reduce conj {} arg)))
+        :ip-community-list           (fn ip-community-list [& arg]
+                                       (assoc {} :ip-community-list (into [] arg)))
+        :ip-as-path-list            (fn ip-as-path-list [& arg]
+                                      (assoc {} :ip-as-path-list (into [] arg)))
+        :neighbor-list            (fn neighbor-list [& arg]
+                                    (assoc {} :neighbor-list (into [] arg)))
+        :hostname                 (fn hostname [& arg]
+                                        (assoc {} :hostname (into [] arg)))
+        :router                   (fn router[& arg]
+                                      (assoc {} :router (into [] arg)))
+        :access-list-record        (fn access-list-record  [& arg]
+                                     (assoc {} :access-list-record (reduce conj {} arg)))
+        :bgp-global                (fn bgp-global  [& arg]
+                                     (assoc {} :bgp-global (reduce conj {} arg)))
+        :neighbor-route-map        (fn neighbor-route-map [& arg]
+                                    (conj [:neighbor-route-map] (reduce conj {} arg)))
+        :neighbor                  (fn neighbor [& arg]
+                                       (assoc {} :neighbor (reduce conj {} arg)))
+        :route-map-set-list        (fn route-map-set-list [& arg]
+                                       (conj [] :route-map-set-list (reduce conj {} arg)))
+        :route-map-record          (fn route-map-record [& arg]
+                                       (conj [] :route-map-record (reduce conj {} arg)))
+        :bgp-confederation          (fn c [& arg]
+                                      (assoc {} :bgp-confederation (reduce conj {} arg)))
+        :confederation-peers        (fn cp [& arg]
+                                      (conj [] :confederation-peers (into #{} (map #(clojure.edn/read-string %) arg))))
+        :confederation-identifier   (fn ci [arg]
+                                       {:confederation-identifier  (clojure.edn/read-string arg)})
+        :device                     (fn device [& arg]
+                                       (assoc {} :device (into [] arg)))}
    input))
