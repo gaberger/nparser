0026fb99c311d3022ce217c14f465ee6eb00ddbe
Gary Berger <58652+gaberger@users.noreply.github.com>
2018-05-20 20:26:01 -0400

    Update interface parser
    Signed-off-by: Gary Berger <58652+gaberger@users.noreply.github.com>

--- src/ncp_parser/core.clj
+++ src/ncp_parser/core.clj
@@ -19,33 +19,51 @@
 
 (defmacro create-parser [device]
     (let [grammar-file (str/join "." [device "ebnf"])
-          grammar (slurp (io/resource (str/join "/" ["parsers" grammar-file])))]
+          grammar (io/resource (str/join "/" ["parsers" grammar-file]))]
       `(defparser  ~(symbol device)
          ~grammar
         :output-format :hiccup
         :string-ci true
         :auto-whitespace whitespace-or-comments)))
 
+(defn debug-transform [& arg]
+  (println "---------------------")
+  (pprint arg)
+  (println (count arg))
+  (println (type arg))
+  (into [] arg))
+  ; (reduce conj [] arg))
 
-
+(mapcat)
 (defn bgp-transform [input]
   (insta/transform
     {
-        :bgprouter                 (fn bgprouter [& arg]
-                                     (assoc {} :bgprouter (reduce conj {} arg)))
-        :asn                       (fn asn [arg]
-                                      (let [asnval (clojure.edn/read-string arg)]
-                                       {:asn asnval}))
-        :bgp-global                (fn c [& arg]
-                                     (assoc {} :bgp-global (reduce conj {} arg)))
-        :bgp-med                   (fn c [& arg]
-                                     (assoc {} :bgp-med (into [] arg)))
-        :bgp-bestpath               (fn c [& arg]
-                                     (assoc {} :bgp-bestpath (into [] arg)))
-        :bgp-confederation          (fn c [& arg]
-                                      (assoc {} :bgp-confederation (reduce conj {} arg)))
-        :confederation-peers        (fn cp [& arg]
-                                      (conj [] :confederation-peers (into #{} (map #(clojure.edn/read-string %) arg))))
-        :confederation-identifier   (fn ci [arg]
-                                       {:confederation-identifier  (clojure.edn/read-string arg)})}
+        :ip-address                (comp #(conj [] :ip-address %) str)
+        :interface                 (fn bgprouter [& arg]
+                                     (reduce conj {} arg))
+        :interface-list            (fn interface-list [& arg]
+                                       (assoc {} :interface-list (into [] arg)))}
+        ; :neighbor                 (fn neighbor [& arg]
+        ;                              (reduce conj {} arg))}
+        ; :neighbor-list            (fn neighbor-list [& arg]
+        ;                             (assoc {} :neighbor-list (into [] arg)))}
+
+
+        ; :bgprouter                 (fn bgprouter [& arg]
+        ;                              (assoc {} :bgprouter (reduce conj {} arg)))
+        ; :asn                       (fn asn [arg]
+        ;                               (let [asnval (clojure.edn/read-string arg)]
+        ;                                {:asn asnval}))
+        ; :bgp-global                (fn c [& arg]
+        ;                              (assoc {} :bgp-global (reduce conj {} arg)))
+        ; :bgp-med                   (fn c [& arg]
+        ;                              (assoc {} :bgp-med (into [] arg)))
+        ; :bgp-bestpath               (fn c [& arg]
+        ;                              (assoc {} :bgp-bestpath (into [] arg)))
+        ; :bgp-confederation          (fn c [& arg]
+        ;                               (assoc {} :bgp-confederation (reduce conj {} arg)))
+        ; :confederation-peers        (fn cp [& arg]
+        ;                               (conj [] :confederation-peers (into #{} (map #(clojure.edn/read-string %) arg))))
+        ; :confederation-identifier   (fn ci [arg]
+        ;                                {:confederation-identifier  (clojure.edn/read-string arg)})}
    input))
