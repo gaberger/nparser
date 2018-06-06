400ed1d04b3cf3be234e448b9fd92f447a4946e0
Gary Berger <58652+gaberger@users.noreply.github.com>
2018-06-06 16:58:53 -0400

    Generator updates
    Signed-off-by: Gary Berger <58652+gaberger@users.noreply.github.com>

--- src/ncp_parser/frr/generator.clj
+++ src/ncp_parser/frr/generator.clj
@@ -12,73 +12,140 @@
 (def options-regex #"^\+")
 
 (defn is-skip-tag? [tag]
-   (some? (re-matches key-regex (name tag))))
+  (some? (re-matches key-regex tag)))
 
 (defn is-transpose-tag? [tag]
-   (some? (re-find #"_" (name tag))))
+  (some? (re-find #"_" tag)))
 
 (defn is-options-tag? [tag]
-  (some? (re-find options-regex (name tag))))
+  (some? (re-find options-regex tag)))
+
+(defn is-option-map? [m]
+  ; {:+<prefix> 0.0.0.0/0, :+le 32}
+  (and (> (count m) 1)
+       (some? (re-find options-regex (name (key (first m)))))))
+
 
 (defn is-regular-tag? [tag]
-  (and  (not (is-skip-tag? tag))
-        (not (is-transpose-tag? tag))
-        (not (is-options-tag? tag))))
+  (and (not (is-skip-tag? tag))
+       (not (is-transpose-tag? tag))
+       (not (is-options-tag? tag))))
 
 (defn transpose-tag [tag]
-    (str (str/join \space (str/split (name tag) #"_")) \space))
+  (str (str/join \space (str/split tag #"_")) \space))
 
 (defn skip-tag [tag]
   (when-not (is-skip-tag? tag)
-            (str (name tag) \space)))
+    (str tag \space)))
 
 (defn regular-tag [tag]
-    (let [tag-string (str (name tag) \space)]
-      (swap! container conj tag-string)))
+  (let [tag-string (str tag \space)]
+    (swap! container conj tag-string)))
 
-; (defn keyword-handler [tag]
+; (defn keyword-handler! [tag]
 ;   (cond
 ;    (is-transpose-tag? tag)  (transpose-tag tag)
 ;    (is-regular-tag? tag)    (regular-tag  tag)))
 
-(defn keyword-handler [tag]
-   (if-let [tag (some->>  tag skip-tag transpose-tag)]
+(defn keyword-handler! [tag]
+  (let [tag-key (name tag)]
+    (if-let [tag (some->> tag-key skip-tag transpose-tag)]
       (do
-        (println "adding keyword -->> " (str (name  tag)))
-        (swap! container conj (str (name  tag))))))
+        (println "adding keyword -->> " tag)
+        (swap! container conj tag)))))
 
-(defn vector-handler [node]
-  (condp #(= (first %2) %1)  node
-    :ip_address   (println "found ip_address tag --> " (rest node))
-    node))
+(defn vector-handler! [node]
+  (condp #(= (first %2) %1) node
+    :ip_address (println "found ip_address tag --> " (rest node))))
 
-(defn set-handler [node]
-   (println "adding set -->> " node)
-   (swap! container conj (str (str/join \space node) \newline)))
+(defn set-handler! [node]
+  (println "adding set -->> " node)
+  (swap! container conj (str (str/join \space node) \newline)))
 
-(defn string-handler [node]
+(defn string-handler! [node]
   (println "adding string -->> " node)
   (swap! container conj (str node \newline)))
 
-(defn number-handler [node]
+(defn number-handler! [node]
   (println "adding number -->> " node)
   (swap! container conj (str node \newline)))
 
-(defn options-handler [node]
+(defn vector-handler! [node]
   (if (and (not (map? (first node)))
            (= (count node) 2))
-      (let [[option value] node]
-        (if (is-options-tag? option)
-          (let [fmt-option (subs (transpose-tag option) 1)]
-               (if
-                 (boolean value)
-                 (do
-                   (println "adding option -->> "  fmt-option)
-                   (swap! container conj (str fmt-option \newline)))
-                 (swap! container conj (str "no " fmt-option \newline)))
-            ; (println "found options tag" (first node)  " --> " (rest node))
-             node))
-        node)))
+    (let [[option value] node
+          option-string (name option)]
+      (if (is-options-tag? option-string)
+        (let [fmt-option (subs (transpose-tag option-string) 1)]
+          (println "adding option -->> " fmt-option value)
+          (cond
+            (boolean? value) (condp = value
+                               true (do (swap! container conj (str fmt-option \newline)) node)
+                               false (do (swap! container conj (str "no " fmt-option \newline)) node))
+            (number? value) (do (swap! container conj (str fmt-option value \newline)) [nil nil])))
+        node))
+    ; true  (swap! container conj (str fmt-option\newline))
+    ; false
+    node))
+
+(defn map-handler [node]
+  (println "executing map-handler " node)
+  ; Handle map options differently as they are variadic.
+  (if
+    (is-option-map? node)
+    (do
+      (println "Processing options map " node)
+      (for [options node]
+        (let [[option value] options
+              option-string (name option)
+              strip-tag (if (is-options-tag? option-string)
+                          (some->> (subs option-string 1)
+                                   skip-tag transpose-tag)
+                          option-string)]
+          (if (nil? strip-tag)
+            (do
+              (println "Adding value ->" value)
+              (swap! container conj (str value \space)))
+            (do
+              (println "Adding option and value ->" option-string value)
+              (swap! container conj (str option-string \space value))))
+          (empty {}))))
+    node))
+
+
+(defn map-handler2 [node]
+  (println "executing map-handler " node)
+  ; Handle map options differently as they are variadic.
+  (if
+    (is-option-map? node)
+    (do
+      (println "Processing options map " node)
+      (let [result (reduce
+                     (fn [acc m]
+                       (let [[option value] m
+                             option-string (name option)
+                             strip-tag (if (is-options-tag? option-string)
+                                         (some->> (subs option-string 1)
+                                                  skip-tag transpose-tag)
+                                         option-string)]
+                         (if (nil? strip-tag)
+                           (conj acc (str value))
+                           (conj acc (str option-string \space value)))))
+                     []
+                     node)
+            retval (first result)
+            _ (println "DEBUG " retval)]
+         (swap! container conj (str retval \newline)))))
+    ;update))
+    ;_ (println "Options map -->>> "  options-map)])
+    ;(empty {}))
+  node)
+;))
+
+; {:<prefix> "0.0.0.0/0" :+le 32}))
+
+; (println "found options tag" (first node)  " --> " (rest node))
+
 
 ; (defn check-vectors [node]
 ;   (condp #(= (first %2) %1) node
@@ -88,48 +155,60 @@
 ;     node))
 
 (defn gen-config [node]
+  (println "Working on node ->" node)
   (cond
-    (string? node)  (string-handler node)
-    (set? #spy/p node) (set-handler node)
-    (number?  node)  (number-handler node)
-    (keyword? node) (keyword-handler node)
-    (vector? node) (options-handler node))
-  node)
+    (string? node) (do (string-handler! node) node)
+    (number? node) (do (number-handler! node) node)
+    (set? node) (do (set-handler! node) (println "NODE-->> " node) #{})
+    (keyword? node) (do (keyword-handler! node) node)
+    (vector? node) (vector-handler! node)
+    (map? node) (map-handler2 node)
+    :else node))
 
 (def model
   {:<device>
-   {:hostname {:<name> "J"}
+   {:hostname         {:<name> "J"}
     :<interfaces>
-              [{:interface {:<name> "eth0", :ip_address "10.0.18.1/24"}}
-               {:interface {:<name> "eth1", :ip_address "10.0.19.1/24"}}
-               {:interface {:<name> "eth2", :ip_address "10.0.15.2/24"}}
-               {:interface {:<name> "eth3", :ip_address "10.0.13.2/24"}}
-               {:interface {:<name> "eth4", :ip_address "10.0.11.2/24"}}
-               {:interface {:<name> "eth5", :ip_address "10.0.9.2/24"}}]
-    :router_bgp {:<asn>           65525,
-                 :+synchronization false
-                 :<bgplist>
-                    [{:bgp  {:router-id       "192.0.0.1",}}
-                     {:bgp  {:+always-compare-med true}}
-                     {:bgp  {:+deterministic-med  true}}
-                     {:bgp  {:bestpath {:+compare-routerid true}}}
-                     {:bgp  {:bestpath {:+as-path_confed true}}}
-                     {:bgp  {:confederation  {:identifier 100}}}
-                     {:bgp  {:confederation  {:peers #{65529 65528 65527 65530},}}}]
-                   :<neighbors>
-                                   [{:neighbor {:10.0.18.2 {:remote-as 200}}}
-                                    {:neighbor {:10.0.19.2 {:remote-as 300}}}
-                                    {:neighbor {:10.0.15.1 {:remote-as              65527
-                                                            :next-hop-self          true
-                                                            :send-community "both"
-                                                            :advertisement-interval "5"}}}
-                                    {:neighbor {:10.0.13.1              {:remote-as 65528}
-                                                :+next-hop-self          true
-                                                :send-community "both"
-                                                :advertisement-interval "5"}}
-                                    {:neighbor {:10.0.18.2 {:<route-maps>
-                                                            [{:route-map {:<policy> "rm-in", :<direction> "in"}}
-                                                             {:route-map {:<policy> "rm-export-2", :<direction> "out"}}]}}}]}}})
+                      [{:interface {:<name> "eth0", :ip_address "10.0.18.1/24"}}
+                       {:interface {:<name> "eth1", :ip_address "10.0.19.1/24"}}
+                       {:interface {:<name> "eth2", :ip_address "10.0.15.2/24"}}
+                       {:interface {:<name> "eth3", :ip_address "10.0.13.2/24"}}
+                       {:interface {:<name> "eth4", :ip_address "10.0.11.2/24"}}
+                       {:interface {:<name> "eth5", :ip_address "10.0.9.2/24"}}]
+    :router_bgp       {:<asn>            65525,
+                       :+synchronization false
+                       :<bgplist>
+                                         [{:bgp {:router-id "192.0.0.1",}}
+                                          {:bgp {:+always-compare-med true}}
+                                          {:bgp {:+deterministic-med true}}
+                                          {:bgp {:bestpath {:+compare-routerid true}}}
+                                          {:bgp {:bestpath {:+as-path_confed true}}}
+                                          {:bgp {:confederation {:identifier 100}}}
+                                          {:bgp {:confederation {:peers #{65529 65528 65527 65530},}}}]
+                       :<neighbors>
+                                         [{:neighbor {:10.0.18.2 {:remote-as 200}}}
+                                          {:neighbor {:10.0.19.2 {:remote-as 300}}}
+                                          {:neighbor {:10.0.15.1 {:remote-as 65527}}}
+                                          {:neighbor {:10.0.15.1 {:+next-hop-self true}}}
+                                          {:neighbor {:10.0.15.1 {:send_community "both"}}}
+                                          {:neighbor {:10.0.15.1 {:advertisement-interval "5"}}}
+                                          {:neighbor {:10.0.13.1 {:remote-as 65528}}}
+                                          {:neighbor {:10.0.13.1 {:+next-hop-self true}}}
+                                          {:neighbor {:10.0.13.1 {:send-community "both"}}}
+                                          {:neighbor {:10.0.13.1 {:advertisement-interval "5"}}}
+                                          {:neighbor {:10.0.18.2 {:route-map {:rm-in "in"}}}}
+                                          {:neighbor {:10.0.18.2 {:route-map {:rm-export-2 "out"}}}}]}
+    :<ip_prefix_list> [{:ip_prefix-list {:pl-1 {:permit {:+<prefix> "1.0.0.0/24"}}}}
+                       {:ip_prefix-list {:pl-2 {:deny {:+<prefix> "1.0.0.0/24"}}}}
+                       {:ip_prefix-list {:pl-3 {:permit {:+<prefix> "1.0.1.0/24"}}}}
+                       {:ip_prefix-list {:pl-4 {:deny {:+<prefix> "1.0.1.0/24"}}}}
+                       {:ip_prefix-list {:pl-5 {:permit {:+<prefix> "2.0.0.0/24"}}}}
+                       {:ip_prefix-list {:pl-6 {:deny {:+<prefix> "2.0.0.0/24"}}}}
+                       {:ip_prefix-list {:pl-7 {:permit {:+<prefix> "2.0.1.0/24"}}}}
+                       {:ip_prefix-list {:pl-8 {:deny {:+<prefix> "2.0.1.0/24"}}}}
+                       {:ip_prefix-list {:pl-9 {:permit {:+<prefix> "0.0.0.0/0" :le 32}}}}
+                       {:ip_prefix-list {:pl-10 {:deny {:+<prefix> "0.0.0.0/0" :le 32}}}}]}})
+
 (defn output_config []
   (postwalk gen-config model)
   (print (str/join @container)))
